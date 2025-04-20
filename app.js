68843fe35cedd54143548b21eb0789796daee524
Yushin Washio <dev@yuwash.eu>
2025-04-20 22:02:15 +0200

    Refactor: Move LearnTab functions to LearnTab component
    Also fixed a few things

--- app.js
+++ app.js
@@ -11,35 +11,117 @@ const EditTab =  {
 
 const LearnTab = {
   view: (vnode) => {
+    const state = vnode.attrs.state;
+
+    const moveToUserSentence = (word, index) => {
+      state.userSentence.push(word);
+      state.availableWords.splice(index, 1);
+      if (state.availableWords.length === 0) {
+        checkOrder();
+      }
+      state.hintWord = null;
+    };
+
+    const moveBackToAvailableWords = (word, index) => {
+      state.availableWords.push(word);
+      state.userSentence.splice(index, 1);
+      state.hintWord = null;
+    };
+
+    const checkOrder = () => {
+      const correct = state.userSentence.join(' ') === state.correctSentence.join(' ');
+      if (correct) {
+        state.score += 1;
+        state.completedSentences++;
+        state.currentSentenceIndex++;
+        vnode.attrs.resetSentence();
+      } else {
+        // Find the index of the first incorrect word
+        let incorrectIndex = 0;
+        for (let i = 0; i < state.userSentence.length; i++) {
+          if (state.userSentence[i] !== state.correctSentence[i]) {
+            incorrectIndex = i;
+            break;
+          }
+        }
+
+        // Move incorrect words back to availableWords
+        for (let i = state.userSentence.length - 1; i >= incorrectIndex; i--) {
+          const word = state.userSentence[i];
+          state.availableWords.push(word);
+          state.userSentence.splice(i, 1);
+        }
+      }
+    };
+
+    const hint = () => {
+      // Move incorrect words back to availableWords
+      for (let i = state.userSentence.length - 1; i >= 0; i--) {
+        if (state.userSentence[i] !== state.correctSentence[i]) {
+          const word = state.userSentence[i];
+          state.availableWords.push(word);
+          state.userSentence.splice(i, 1);
+        }
+      }
+
+      // Highlight the next correct word
+      if (state.userSentence.length < state.correctSentence.length) {
+        state.hintWord = state.correctSentence[state.userSentence.length];
+      }
+    };
+
+    const restart = () => {
+      state.sentences = state.text.match(/[^.!?]+[.!?]+/g) || [];
+      state.sentences = state.sentences.map(sentence => sentence.trim().split(/\s+/)).reduce(
+        (acc, current) => {
+          if (current.length < 5 && 0 < acc.length) {
+            acc[acc.length - 1] = acc[acc.length - 1].concat(current);
+          } else if (0 < current.length) {
+            acc.push(current);
+          }
+          return acc;
+        },
+        []
+      );
+      state.currentSentenceIndex = 0;
+      vnode.attrs.resetSentence();
+      state.completedSentences = 0;
+      state.message = '';
+    };
+
+    const progress = 0 < state.sentences.length ? (
+      (state.completedSentences / state.sentences.length) * 100
+    ): 0;
+
     return m('div', [
-      m('progress', { class: 'progress is-success', value: vnode.attrs.progress, max: '100' }, `${vnode.attrs.progress}%`),
+      m('progress', { class: 'progress is-success', value: progress, max: '100' }, `${progress}%`),
       m('div', { class: 'mb-4' }, [
-        m('div', { class: 'flex flex-wrap gap-2 mb-2' }, vnode.attrs.availableWords.map((word, index) =>
+        m('div', { class: 'flex flex-wrap gap-2 mb-2' }, state.availableWords.map((word, index) =>
           m('button', {
-            class: `button ${word === vnode.attrs.hintWord ? 'is-success' : 'is-info'} mr-1`,
-            onclick: () => vnode.attrs.moveToUserSentence(word, index)
+            class: `button ${word === state.hintWord ? 'is-success' : 'is-info'} mr-1`,
+            onclick: () => moveToUserSentence(word, index)
           }, word)
         )),
-        m('div', { class: 'flex flex-wrap gap-2 mb-2' }, vnode.attrs.userSentence.map((word, index) =>
+        m('div', { class: 'flex flex-wrap gap-2 mb-2' }, state.userSentence.map((word, index) =>
           m('button', {
             class: 'button is-success mr-1',
-            onclick: () => vnode.attrs.moveBackToAvailableWords(word, index)
+            onclick: () => moveBackToAvailableWords(word, index)
           }, word)
         ))
       ]),
       m('div', { class: 'field is-grouped mb-4' }, [
         m('button', {
           class: 'button control is-link',
-          onclick: () => vnode.attrs.hint(),
-          disabled: vnode.attrs.correctSentence.length === 0 || vnode.attrs.hintWord !== null
+          onclick: hint,
+          disabled: state.correctSentence.length === 0 || state.hintWord !== null
         }, 'Hint'),
         m('button', {
           class: 'button control is-warning',
-          onclick: () => vnode.attrs.restart(),
-          disabled: vnode.attrs.sentences.length === 0
+          onclick: restart,
+          disabled: state.sentences.length === 0
         }, 'Restart'),
       ]),
-      m('p', `Score: ${vnode.attrs.score}`)
+      m('p', `Score: ${state.score}`),
     ]);
   }
 };
@@ -64,9 +146,6 @@ const app = {
   },
   setText: function(event) {
     this.state.text = event.target.value;
-    this.processText();
-  },
-  processText: function() {
     const sentences = this.state.text.match(/[^.!?]+[.!?]+/g) || [];
     this.state.sentences = sentences.map(sentence => sentence.trim().split(/\s+/)).reduce(
       (acc, current) => {
@@ -79,96 +158,16 @@ const app = {
       },
       []
     );
-    console.log(this.state.sentences);
     this.state.currentSentenceIndex = 0;
+    this.resetSentence();
+    this.state.completedSentences = 0;
+  },
+  resetSentence: function() {
     this.state.correctSentence = this.state.sentences[this.state.currentSentenceIndex] || [];
     this.state.availableWords = [...this.state.correctSentence]; // Store the correct sentence
-    this.shuffleSentence();
-    this.state.completedSentences = 0;
-    this.state.hintWord = null;
-  },
-  shuffleSentence: function() {
     this.state.availableWords.sort();
     this.state.userSentence = [];
-  },
-  moveToUserSentence: function(word, index) {
-    this.state.userSentence.push(word);
-    this.state.availableWords.splice(index, 1);
-    if (this.state.availableWords.length === 0) {
-      this.checkOrder();
-    }
     this.state.hintWord = null;
-  },
-  moveBackToAvailableWords: function(word, index) {
-    this.state.availableWords.push(word);
-    this.state.userSentence.splice(index, 1);
-    this.state.hintWord = null;
-  },
-  hint: function() {
-     // Move incorrect words back to availableWords
-     for (let i = this.state.userSentence.length - 1; i >= 0; i--) {
-       if (this.state.userSentence[i] !== this.state.correctSentence[i]) {
-         const word = this.state.userSentence[i];
-         this.state.availableWords.push(word);
-         this.state.userSentence.splice(i, 1);
-       }
-     }
-
-     // Highlight the next correct word
-     if (this.state.userSentence.length < this.state.correctSentence.length) {
-       this.state.hintWord = this.state.correctSentence[this.state.userSentence.length];
-     }
-   },
-   restart: function() {
-      this.state.sentences = this.state.text.match(/[^.!?]+[.!?]+/g) || [];
-      this.state.sentences = this.state.sentences.map(sentence => sentence.trim().split(/\s+/)).reduce(
-        (acc, current) => {
-          if (current.length < 5 && 0 < acc.length) {
-            acc[acc.length - 1] = acc[acc.length - 1].concat(current);
-          } else if (0 < current.length) {
-            acc.push(current);
-          }
-          return acc;
-        },
-        []
-      );
-      this.state.currentSentenceIndex = 0;
-      this.state.correctSentence = this.state.sentences[this.state.currentSentenceIndex] || [];
-      this.state.availableWords = [...this.state.correctSentence];
-      this.shuffleSentence();
-      this.state.message = '';
-    },
-  checkOrder: function() {
-    const correct = this.state.userSentence.join(' ') === this.state.correctSentence.join(' ');
-    if (correct) {
-      this.state.score += 1;
-      this.state.completedSentences++;
-      this.state.currentSentenceIndex++;
-      this.state.correctSentence = this.state.sentences[this.state.currentSentenceIndex] || [];
-      this.state.availableWords = [...this.state.correctSentence]; // Store the correct sentence
-      if (this.state.correctSentence.length === 0) {
-        this.state.message = 'Congratulations! You have completed the exercise!';
-      } else {
-        this.shuffleSentence();
-      }
-    } else {
-      console.log('Incorrect', this.state.userSentence, this.state.correctSentence);
-      // Find the index of the first incorrect word
-      let incorrectIndex = 0;
-      for (let i = 0; i < this.state.userSentence.length; i++) {
-        if (this.state.userSentence[i] !== this.state.correctSentence[i]) {
-          incorrectIndex = i;
-          break;
-        }
-      }
-
-      // Move incorrect words back to availableWords
-      for (let i = this.state.userSentence.length - 1; i >= incorrectIndex; i--) {
-        const word = this.state.userSentence[i];
-        this.state.availableWords.push(word);
-        this.state.userSentence.splice(i, 1);
-      }
-    }
   },
   view: function() {
     let message = '';
@@ -176,18 +175,17 @@ const app = {
       message = this.state.text === '' ? 'Enter text to get started!' : 'Go to the ‘Learn’ tab to start learning!';
     } else if (this.state.activeTab === 'learn') {
       message = (
-        this.state.correctSentence.length === 0 ?
-        'Please go to the ‘edit’ tab to get started!' :
-        'Click on the words in the right order to make the sentence.'
+        this.state.sentences.length === 0 ?
+        'Please go to the ‘edit’ tab to get started!' : (
+          this.state.completedSentences < this.state.sentences.length ?
+          'Click on the words in the right order to make the sentence.':
+          'Congratulations! You have completed the exercise!'
+        )
       );
     }
     if (this.state.message) {
       message = this.state.message;
     }
-
-    const progress = 0 < this.state.sentences.length ? (
-      (this.state.completedSentences / this.state.sentences.length) * 100
-    ): 0;
 
     return m('div', { class: 'section' }, [
       m('div', { class: 'tabs is-centered is-boxed' },
@@ -209,17 +207,8 @@ const app = {
           : null,
         this.state.activeTab === 'learn' ?
           m(LearnTab, {
-            progress: progress,
-            availableWords: this.state.availableWords,
-            userSentence: this.state.userSentence,
-            correctSentence: this.state.correctSentence,
-            hintWord: this.state.hintWord,
-            moveToUserSentence: this.moveToUserSentence.bind(this),
-            moveBackToAvailableWords: this.moveBackToAvailableWords.bind(this),
-            hint: this.hint.bind(this),
-            restart: this.restart.bind(this),
-            sentences: this.state.sentences,
-            score: this.state.score
+            state: this.state,
+            resetSentence: this.resetSentence.bind(this),
           })
           : null
       ]),
